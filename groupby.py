fae44b5073a1d5ecab9ea72d400afd7fd07423b3
Jonathan Stray <jonathanstray@gmail.com>
2017-10-04 17:31:45 -0400

    Count should not require a target column.

--- groupby.py
+++ groupby.py
@@ -1,4 +1,5 @@
 import numpy as np
+import pandas as pd
 
 class Importable:
     @staticmethod
@@ -17,18 +18,22 @@ class Importable:
         targetcolumn = wf_module.get_param_column('targetcolumn')
 
         # while the user has not provided all the parameters, returns full table.
-        if targetcolumn == '':
+        if targetcolumn == '' and operation != 0:  # process without target column if counting
             wf_module.set_ready(notify=False)
             return table
         else:
             # if target column is not a numeric type, tries to convert it (before any aggregation)
-            if table[targetcolumn].dtype != np.float64 and table[targetcolumn].dtype != np.int64:
+            if targetcolumn != '' and (table[targetcolumn].dtype != np.float64 and table[targetcolumn].dtype != np.int64:
                 table[targetcolumn] = table[targetcolumn].str.replace(',', '')
                 table[targetcolumn] = table[targetcolumn].astype(float)
             # if operation must be performed on the entire table...
             if groupby == '':
                 if operation == 0: # count
-                    newtab = table[[targetcolumn]].count().to_frame()
+                    if targetcolumn == '':
+                        # count with no groupby and no target just counts rows in table
+                        newtab = pd.DataFrame([len(table)], columns=['count'])  
+                    else:
+                        newtab = table[[targetcolumn]].count().to_frame()
                 elif operation == 1: # average
                     newtab = table[[targetcolumn]].mean().to_frame()
                 elif operation == 2: # sum
