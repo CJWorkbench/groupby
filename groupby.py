84056ea0023ababd651a2fbd13291efd8801af70
Jonathan Stray <jonathanstray@gmail.com>
2017-11-14 20:02:30 -0500

    Update to no-boilerplate format

--- groupby.py
+++ groupby.py
@@ -1,44 +1,31 @@
-import numpy as np
-import pandas as pd
+def render(wf_module, table):
+    # obtain parameters
+    groupby = wf_module.get_param_column('groupby')
+    operation = wf_module.get_param_menu_idx("operation")
+    targetcolumn = wf_module.get_param_column('targetcolumn')
 
-class Importable:
-    @staticmethod
-    def __init__(self):
-        pass
+    # while the user has not provided all the parameters, returns full table.
+    if groupby == '' or (targetcolumn == '' and operation != 0):  # process without target column if counting
+        wf_module.set_ready(notify=False)
+        return table
+    else:
+        # if target column is not a numeric type, tries to convert it (before any aggregation)
+        if targetcolumn != '' and (table[targetcolumn].dtype != np.float64 and table[targetcolumn].dtype != np.int64):
+            table[targetcolumn] = table[targetcolumn].str.replace(',', '')
+            table[targetcolumn] = table[targetcolumn].astype(float)
 
-    @staticmethod
-    def event():
-        pass
+        if operation == 0: # count
+            newtab = table.groupby([groupby])[[groupby]].count()
+            newtab.columns = ['count'] # otherwise index name and count column name are the same, error on reset_index below
+        elif operation == 1: # average
+            newtab = table.groupby([groupby])[[targetcolumn]].mean()
+        elif operation == 2: # sum
+            newtab = table.groupby([groupby])[[targetcolumn]].sum()
+        elif operation == 3: # min
+            newtab = table.groupby([groupby])[[targetcolumn]].min()
+        elif operation == 4: # max
+            newtab = table.groupby([groupby])[[targetcolumn]].max()
+        newtab.reset_index(level=0, inplace=True)
 
-    @staticmethod
-    def render(wf_module, table):
-        # obtain parameters
-        groupby = wf_module.get_param_column('groupby')
-        operation = wf_module.get_param_menu_idx("operation")
-        targetcolumn = wf_module.get_param_column('targetcolumn')
-
-        # while the user has not provided all the parameters, returns full table.
-        if groupby == '' or (targetcolumn == '' and operation != 0):  # process without target column if counting
-            wf_module.set_ready(notify=False)
-            return table
-        else:
-            # if target column is not a numeric type, tries to convert it (before any aggregation)
-            if targetcolumn != '' and (table[targetcolumn].dtype != np.float64 and table[targetcolumn].dtype != np.int64):
-                table[targetcolumn] = table[targetcolumn].str.replace(',', '')
-                table[targetcolumn] = table[targetcolumn].astype(float)
-           
-            if operation == 0: # count
-                newtab = table.groupby([groupby])[[groupby]].count()
-                newtab.columns = ['count'] # otherwise index name and count column name are the same, error on reset_index below
-            elif operation == 1: # average
-                newtab = table.groupby([groupby])[[targetcolumn]].mean()
-            elif operation == 2: # sum
-                newtab = table.groupby([groupby])[[targetcolumn]].sum()
-            elif operation == 3: # min
-                newtab = table.groupby([groupby])[[targetcolumn]].min()
-            elif operation == 4: # max
-                newtab = table.groupby([groupby])[[targetcolumn]].max()
-            newtab.reset_index(level=0, inplace=True)
-            
-            wf_module.set_ready(notify=False)
-            return newtab
+        wf_module.set_ready(notify=False)
+        return newtab
