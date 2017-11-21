fe9eea3fd417bd33fbf5dd0163ab426a6c01ef54
Jonathan Stray <jonathanstray@gmail.com>
2017-11-20 17:42:33 -0800

    Conform to new API

--- groupby.py
+++ groupby.py
@@ -1,12 +1,11 @@
-def render(wf_module, table):
+def render(table, parmas):
     # obtain parameters
-    groupby = wf_module.get_param_column('groupby')
-    operation = wf_module.get_param_menu_idx("operation")
-    targetcolumn = wf_module.get_param_column('targetcolumn')
+    groupby = params['groupby']
+    operation = params['operation']
+    targetcolumn = params['targetcolumn']
 
     # while the user has not provided all the parameters, returns full table.
     if groupby == '' or (targetcolumn == '' and operation != 0):  # process without target column if counting
-        wf_module.set_ready(notify=False)
         return table
     else:
         # if target column is not a numeric type, tries to convert it (before any aggregation)
@@ -27,5 +26,4 @@ def render(wf_module, table):
             newtab = table.groupby([groupby])[[targetcolumn]].max()
         newtab.reset_index(level=0, inplace=True)
 
-        wf_module.set_ready(notify=False)
         return newtab
